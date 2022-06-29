7f58d789e7d2b4fec8f41328b4c9d0610dd42563
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-06-29 22:29:41 +0300

    Update sum_of_two.c

--- .github/workflows/sum_of_two.c
+++ .github/workflows/sum_of_two.c
@@ -12,5 +12,9 @@ int main(int quantity, char  **number) {
     sum=1;
 
     printf("%s %s", number[1], number[2]);
+    int num1 = number[1] - '0'; 
+    int num2 = number[2] - '0'; 
+    sum = num1 + num2;
+    printf("sum = %d" , sum );
     return 0;
 }
