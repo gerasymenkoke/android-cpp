7eb97a97b4f14a26442973707355bd4e28d7fa58
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2022-06-30 22:22:40 +0300

    Update sum_of_two.c

--- .github/workflows/sum_of_two.c
+++ .github/workflows/sum_of_two.c
@@ -13,7 +13,7 @@ int main(int quantity, char  **number) {
     // sum = number1 + number2;      
     sum=1;
 
-    printf("%s %s", number[1], number[2]);
+    // printf("%s %s", number[1], number[2]);
     int num1 = atoi(number[1]); 
     int num2 = atoi(number[2]); 
     sum = num1 + num2;
